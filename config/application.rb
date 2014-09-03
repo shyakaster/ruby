9f0ea2cf2277a1a0dc298ec9a7b7726bdbd3f88d
shyakaster@gmail.com <shyakaster@gmail.com>
2014-09-03 22:33:33 +0300

    Changed config/application file

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,14 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# config/application.rb
+
+
 Bundler.require(*Rails.groups)
 
 module FirstApp2
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
