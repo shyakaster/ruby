75fab308e03038ec3c157d425929c1ceef284ad0
shyakaster@gmail.com <shyakaster@gmail.com>
2014-09-03 22:46:40 +0300

    Added Sqlite3 into development in the Gemfile

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 # config/application.rb
-config.assets.initialize_on_precompile = false
+
 
 Bundler.require(*Rails.groups)
 
 module FirstApp2
   class Application < Rails::Application
-    
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
